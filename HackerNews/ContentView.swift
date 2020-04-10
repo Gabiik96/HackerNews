d5c1b7c9f27d2ce7bf4c868c5b420ada56118644
Gabriel Balta <g.balta96@gmail.com>
2020-04-10 21:22:24 +0100

    Initial commit with structure for list and title at list

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by Gabriel Balta on 10/04/2020.
+//  Copyright © 2020 Gabriel Balta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+            }
+        .navigationBarTitle("Hacker news")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola")
+]
