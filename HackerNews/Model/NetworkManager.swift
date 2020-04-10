391eb73687e723ccda184bdf4d6fe760705e596a
Gabriel Balta <g.balta96@gmail.com>
2020-04-10 21:26:44 +0100

    Update file hierarchy with "Model" group & new file .swift for networking api

---
+++ HackerNews/Model/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  HackerNews
+//
+//  Created by Gabriel Balta on 10/04/2020.
+//  Copyright © 2020 Gabriel Balta. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+}
