c56ab0fe9606c3fdb961b607f7b0ad0e1d0fd795
Gabriel Balta <g.balta96@gmail.com>
2020-04-10 22:07:19 +0100

    new .swiftUI file "ContentView", connected to each post by NavigationLink clickable cells.

--- HackerNews/View/ContentView.swift
+++ HackerNews/View/ContentView.swift
@@ -10,18 +10,20 @@ import SwiftUI
 
 
 struct ContentView: View {
-
+    
     @ObservedObject var networkManager = NetworkManager()
     
     var body: some View {
         NavigationView {
             List(networkManager.posts) { post in
-                HStack {
-                    Text(String(post.points))
-                    Text(post.title)
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
                 }
             }
-        .navigationBarTitle("HACKER NEWS")
+            .navigationBarTitle("HACKER NEWS")
         }
         .onAppear {
             self.networkManager.fetchData()
---
+++ HackerNews/View/DetailView.swift
@@ -0,0 +1,24 @@
+//
+//  DetailView.swift
+//  HackerNews
+//
+//  Created by Gabriel Balta on 10/04/2020.
+//  Copyright © 2020 Gabriel Balta. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://google.com")
+    }
+}
